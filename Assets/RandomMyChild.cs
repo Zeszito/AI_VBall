0a6afba3698ce99a4cca96b96ff9075f742a5226
Moisés <liricractos@gmail.com>
2022-07-26 07:08:58 +0100

    HE DID IT!
    HE DID IT!

---
+++ Assets/RandomMyChild.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMyChild : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public List<Transform> myChilds;
+
+    void Start()
+    {
+        myChilds.AddRange(gameObject.GetComponentsInChildren<Transform>());
+    }
+
+    public void RandMyChild()
+    {
+        int stRage = -60;
+        int finalrang = 15;
+        foreach (Transform t in myChilds)
+        {
+            t.gameObject.SetActive(true);
+        }
+
+            foreach (Transform t in myChilds)
+        {
+            foreach (Transform f in myChilds)
+            {
+                if (t.gameObject.tag == "dead" && f.gameObject.tag == "dead" && t.gameObject != this.gameObject && f.gameObject != this.gameObject && t.gameObject != f.gameObject)
+                {
+                    float po = Random.Range(stRage, finalrang);
+                
+                    t.localPosition = new Vector3(po , t.localPosition.y, t.localPosition.z);
+                    
+                    if (t.GetComponent<BoxCollider>().bounds.Intersects((f.GetComponent<BoxCollider>().bounds)) && t.gameObject != f.gameObject){
+                        f.gameObject.SetActive(false);
+                    }
+                    if ( Mathf.Abs(f.localPosition.x- t.localPosition.x) < 1 && t.gameObject != f.gameObject && t.gameObject != this.gameObject && f.gameObject != this.gameObject)
+                    {
+                        f.gameObject.SetActive(false);
+                    }
+
+
+                }
+                
+                
+            }
+        }
+
+  
+
+    }
+
+    public void RandDeact()
+    {
+        foreach (Transform t in myChilds)
+        {
+            t.gameObject.SetActive(true);
+        }
+
+        foreach (Transform t in myChilds)
+        {
+            int Rnd = Random.Range(1, 6);
+            if(Rnd == 1 && t.gameObject.tag == "dead")
+            {
+                t.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Agent_not_fit.cs
@@ -0,0 +1,179 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Agent_not_fit : Agent
+{
+    public Rigidbody rgBody;
+    public float force;
+    public float forceh;
+    public float force3x;
+
+
+    public GameObject win;
+    public GameObject obsParent;
+    public Transform[] obs;
+
+    public Quaternion myInrot;
+    public Vector3 myInPos;
+
+    public RandomMyChild RandomMyChild;
+
+    //public Text wintxt;
+    //int winVal = 0;
+    //public Text losetxt;
+    //int looseVal = 1;
+
+    //para eu mexer
+    public void Start()
+    {
+
+        obs = obsParent.GetComponentsInChildren<Transform>();
+        myInrot = this.transform.localRotation;
+        myInPos = this.transform.localPosition;
+        this.rgBody.transform.Rotate(new Vector3(0, 0, Random.Range(0, 360f)));
+        RandomMyChild.RandDeact();
+        if (forceh == 0)
+        {
+            forceh = force / Random.Range(1, 3);
+        }
+   
+        if (force3x == 0)
+        {
+            force3x = force * Random.Range(1,5);
+        }
+
+       // wintxt.text = winVal.ToString();
+        //losetxt.text = looseVal.ToString();
+
+    }
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var contAct = actionsOut.ContinuousActions;
+        contAct[0] = Input.GetAxis("Horizontal") != 0 ? Input.GetAxis("Horizontal") : 0;
+        contAct[1] = Input.GetAxis("Vertical") != 0 ? Input.GetAxis("Vertical") : 0;
+
+
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+
+        // base.OnActionReceived(actions);
+        float z = actions.ContinuousActions[0] * force3x;
+
+        float x = actions.ContinuousActions[1] * forceh;
+
+
+
+
+        this.rgBody.AddForce(new Vector3(x, 0, 0), ForceMode.Impulse);
+
+
+
+
+        this.rgBody.transform.Rotate(new Vector3(0, 0, z));
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        this.transform.localPosition = myInPos;
+        this.transform.localRotation = myInrot;
+        this.rgBody.transform.Rotate(new Vector3(0, 0, Random.Range(0,360f)));
+        RandomMyChild.RandDeact();
+        
+        
+            forceh = force / Random.Range(1, 3);
+        
+
+        
+        
+            force3x = force * Random.Range(1, 5);
+        
+        // wintxt.text = winVal.ToString();
+        //losetxt.text = looseVal.ToString();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(this.transform.rotation.x);
+
+
+        // Vector from agent to goal
+        Vector3 toWin = new Vector3((win.transform.localPosition.x - this.transform.localPosition.x),
+        (win.transform.localPosition.y - this.transform.localPosition.y),
+        (win.transform.localPosition.z - this.transform.localPosition.z));
+
+
+        //vectorAgent to goal (3)
+        sensor.AddObservation(toWin.normalized);
+
+        // Distance from the goal (1 float)
+        sensor.AddObservation(toWin.magnitude);
+
+
+        //obestacles
+        foreach (Transform o in obs) //7 * 16
+        {
+            Vector3 toO = new Vector3((o.localPosition.x - this.transform.localPosition.x),
+            (o.localPosition.y - this.transform.localPosition.y),
+            (o.localPosition.z - this.transform.localPosition.z));
+
+
+            //vectorAgent to goal
+            sensor.AddObservation(toO.normalized);//3
+
+            // Distance from the goal (1 float)
+            sensor.AddObservation(toO.magnitude);//1
+
+            //Rotarion
+            sensor.AddObservation(o.localRotation.x);//1
+            sensor.AddObservation(o.localRotation.y);//1
+            sensor.AddObservation(o.localRotation.z);//1
+        }
+
+
+        // Agent velocity (3 floats)
+        sensor.AddObservation(rgBody.velocity);
+
+
+    }
+
+    //collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "dead")
+        {
+            AddReward(-0.6f);
+          //  looseVal++;
+            EndEpisode();
+        }
+        if (collision.gameObject.tag == "prize")
+        {
+            AddReward(1);
+           // winVal++;
+            EndEpisode();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bonus")
+        {
+            AddReward(0.5f);
+
+        }
+        if (other.gameObject.tag == "prize")
+        {
+            AddReward(1);
+            EndEpisode();
+        }
+    }
+    //help
+
+}
